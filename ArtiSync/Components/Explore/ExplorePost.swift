7b9d7abe8cd802e6470bee1004e4870ae1623293
President Raindas <presidentraindas@gmail.com>
2023-11-21 16:35:04 +0100

    Explore view completed

--- ArtiSync/Components/Explore/ExplorePost.swift
+++ ArtiSync/Components/Explore/ExplorePost.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct ExplorePost: View {
+    
+    let postImg:String
+    let userDP:String
+    let title:String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            AsyncImage(url: URL(string: postImg)) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            
+            HStack {
+                AsyncImage(url: URL(string: postImg)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 25, height: 25)
+                .clipShape(Circle())
+                
+                Text(title)
+                    .foregroundColor(.primary)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+            }.padding()
+        }
+        .frame(height: 200)
+        .background()
+        .cornerRadius(10)
+        .shadow(radius: 2)
     }
 }
 
 struct ExplorePost_Previews: PreviewProvider {
     static var previews: some View {
-        ExplorePost()
+        ExplorePost(postImg: "https://cdn.pixabay.com/audio/2023/08/31/14-35-42-339_200x200.jpg", userDP: "https://cdn.pixabay.com/audio/2023/08/31/14-35-42-339_200x200.jpg", title: "FLORA landing page")
     }
 }
