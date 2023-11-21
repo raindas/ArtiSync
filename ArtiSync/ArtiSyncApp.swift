a121c42a676b763fe858d3f04cea946becdeba86
President Raindas <presidentraindas@gmail.com>
2023-11-21 01:41:53 +0100

    Initial Commit

---
+++ ArtiSync/ArtiSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtiSyncApp.swift
+//  ArtiSync
+//
+//  Created by President Raindas on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtiSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
