3816bf58e3cb3efa021d9920677c5983d8aae63d
President Raindas <presidentraindas@gmail.com>
2023-11-21 16:08:10 +0100

    Explore individual posts

---
+++ ArtiSync/Components/Explore/ExplorePost.swift
@@ -0,0 +1,20 @@
+//
+//  ExplorePost.swift
+//  ArtiSync
+//
+//  Created by President Raindas on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ExplorePost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExplorePost_Previews: PreviewProvider {
+    static var previews: some View {
+        ExplorePost()
+    }
+}
