d00801551e25e6e1e62cddbea4820564b98608e6
President Raindas <presidentraindas@gmail.com>
2023-11-21 20:40:46 +0100

    messages view

---
+++ ArtiSync/Main/Messages/Views/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  ArtiSync
+//
+//  Created by President Raindas on 21/11/2023.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
