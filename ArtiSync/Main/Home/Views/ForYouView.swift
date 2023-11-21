c63842d1ee5b9243aa3c1cd80de37a590442ce1e
President Raindas <presidentraindas@gmail.com>
2023-11-21 01:51:39 +0100

    Home view - for you view

---
+++ ArtiSync/Main/Home/Views/ForYouView.swift
@@ -0,0 +1,22 @@
+//
+//  ForYouView.swift
+//  ArtiSync
+//
+//  Created by President Raindas on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ForYouView: View {
+    var body: some View {
+        ScrollView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ForYouView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForYouView()
+    }
+}
