33b3517c88a8e8f20b8e3788bcc3226b5283c719
President Raindas <presidentraindas@gmail.com>
2023-11-22 02:23:43 +0100

    Authentication view model

---
+++ ArtiSync/Main/Auth/ViewModels/AuthVM.swift
@@ -0,0 +1,20 @@
+//
+//  AuthVM.swift
+//  ArtiSync
+//
+//  Created by President Raindas on 22/11/2023.
+//
+
+import SwiftUI
+
+struct AuthVM: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthVM_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthVM()
+    }
+}
