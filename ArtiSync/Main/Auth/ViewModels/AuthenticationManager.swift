867413506debdba4f85e51ea723ee614007d0dac
President Raindas <presidentraindas@gmail.com>
2023-11-22 22:21:52 +0100

    Update AuthenticationManager.swift

--- ArtiSync/Main/Auth/ViewModels/AuthenticationManager.swift
+++ ArtiSync/Main/Auth/ViewModels/AuthenticationManager.swift
@@ -19,11 +19,20 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: authDataResult.user)
     }
     
+    func signInUser(email:String, pass:String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: pass)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
     func getAuthenticatedUser() throws -> AuthDataResultModel {
         guard let user = Auth.auth().currentUser else {
             throw URLError(.badServerResponse)
         }
         
         return AuthDataResultModel(user: user)
+    }
+    
+    func signOut() throws {
+        try Auth.auth().signOut()
     }
 }
